4ee29b3dce5a8095bf682b8872a60c02619e504a
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-12-24 19:39:06 +0530

    add json jsonservice

--- src/organized-data/json/json.service.ts
+++ src/organized-data/json/json.service.ts
@@ -1,10 +1,60 @@
-import { Injectable,Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { RefineParams } from './types/types';
+import { LlmService } from '../llm/llm.service';
+import { Model } from '../llm/types/types';
+import { jsonZeroShotSchemaExtraction } from './prompts';
+import { InvalidJsonOutputError } from './exceptions/exception';
+
 
 @Injectable()
 export class JsonService {
 
-    private readonly logger = new Logger(JsonService.name);
+    private defaultRefineParams: RefineParams ={
+        chunkSize: 2000,
+        overlap: 100
+    };
+    
+    constructor( private llmService: LlmService){}
+
+
+
+
+    async extractWithSchema(
+        text: string,
+        model: Model,
+        schema: string,
+        debug: false,
+    ){
+        
+        const { output,debugReport}= await this.llmService.generateOutput(
+            model,
+            jsonZeroShotSchemaExtraction,
+            {
+                context: text,
+                jsonSchema: schema
+            },
+            debug
+        );
+
+         try {
+
+            const json: object =JSON.parse(output.text);
+
+            return {
+                json,
+                debugReport
+            };
+            
+         } catch (err) {
+            throw new InvalidJsonOutputError()
+         }
+    };
+
 
     
+    
 
-}
+
+
+
+};
