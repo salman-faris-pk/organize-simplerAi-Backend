bb491d545d1600ae32833dfaca0c6f637c579169
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-12-30 11:36:30 +0530

    almost done

--- src/cron.service.ts
+++ src/cron.service.ts
@@ -3,21 +3,34 @@ import { Cron } from '@nestjs/schedule';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { ISOLogger } from './logger/iso-logger.service';
+import { ConfigService } from "@nestjs/config"
+
+
 
 @Injectable()
 export class CronService {
+   
+  private readonly url: string
+
   constructor(
     private readonly httpService: HttpService,
     private logger: ISOLogger,
+    private configService: ConfigService,
 ) {
-    this.logger.setContext(CronService.name);
+    this.logger.setContext(CronService.name)
+     const nodeEnv = this.configService.get<string>('NODE_ENV')
+     this.url =
+      nodeEnv === 'production'
+        ? this.configService.get<string>('HEALTH_CHECK_URL')!
+        : 'http://localhost:3000/';
 }
+
 
   @Cron('*/14 * * * *')
   async triggerHealthCheck() {
     try {
        const response=await firstValueFrom(
-        this.httpService.get('http://localhost:3000/')
+        this.httpService.get(this.url)
       );
 
       this.logger.debug(`Health check successful: ${response.status}`)
