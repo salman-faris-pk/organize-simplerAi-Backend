be35983d69931aab8e89d657eb5568dd8ec45677
salmanul faris pk <salmanulfarispk2001@gmail.com>
2026-01-07 15:49:14 +0530

    finished done!

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,24 +4,29 @@ import { Redis } from 'ioredis';
 import * as schema from 'src/database/schema';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
-
+const UUID_REGEX =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const API_KEY_TTL = 60 * 15;
 const API_KEY_PREFIX = 'apikey:';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @Inject('DRIZZLE_CLIENT') private readonly db: NodePgDatabase<typeof schema>,
+    @Inject('DRIZZLE_CLIENT')
+    private readonly db: NodePgDatabase<typeof schema>,
     @Inject('REDIS_CLIENT') private readonly redis: Redis,
   ) {}
 
   async validateApiKey(apiKey: string) {
+    if (!UUID_REGEX.test(apiKey)) {
+      return false;
+    }
 
     const cacheKey = `${API_KEY_PREFIX}${apiKey}`;
-    
+
     if (await this.redis.exists(cacheKey)) {
       return true;
-    };
+    }
 
     const result = await this.db.execute(
       sql`
@@ -33,13 +38,12 @@ export class AuthService {
     `,
     );
 
-     if (result.rowCount === 0) {
+    if (result.rowCount === 0) {
       return false;
-     };
-     
-     await this.redis.setex(cacheKey, API_KEY_TTL, '1');
+    }
 
-     return true;
+    await this.redis.setex(cacheKey, API_KEY_TTL, '1');
 
+    return true;
   }
 }
