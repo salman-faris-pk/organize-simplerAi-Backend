e6a8b9f3a5c31eb96e1a15dfa78234e03cdf6916
salmanul faris pk <salmanulfarispk2001@gmail.com>
2026-01-23 23:50:30 +0530

    chnages in pdfparser api

--- src/parsers/pdf-parser/pdf-parser.service.ts
+++ src/parsers/pdf-parser/pdf-parser.service.ts
@@ -3,40 +3,41 @@ import { HttpService } from '@nestjs/axios';
 import { PdfNotParsedError, PdfSizeError } from './exceptions/exceptions';
 import * as pdfjs from 'pdfjs-dist/build/pdf';
 
+
 @Injectable()
 export class PdfParserService {
   constructor(private httpService: HttpService) {}
 
-  async parsePdf(file: Buffer) {
-    const loadingTask = pdfjs.getDocument({ data: new Uint8Array(file) });
-    const pdf = await loadingTask.promise;
 
-    let text = '';
+async parsePdf(file: Buffer): Promise<string> {
+  const loadingTask = pdfjs.getDocument({ data: new Uint8Array(file) });
+  const pdf = await loadingTask.promise;
 
-    for (let pageNo = 1; pageNo <= pdf.numPages; pageNo++) {
-      const page = await pdf.getPage(pageNo);
-      const content = await page.getTextContent();
+  let text = '';
 
-      text += content.items.map((item: any) => item.str).join(' ') + '\n';
+  for (let pageNo = 1; pageNo <= pdf.numPages; pageNo++) {
+    const page = await pdf.getPage(pageNo);
+    const content = await page.getTextContent();
+
+    const pageText = content.items 
+      .map((item: any) => item.str.trim())  //create a new array where each element is the str of the item and removes both ends whitespace.
+      .filter(Boolean)   //Removes any empty strings from the array.
+      .join(' ');
+
+    if (pageText) {
+      const cleaned = pageText.replace(/\s{3,}/g, '   ');
+
+      text += cleaned + `\n---PAGE ${pageNo}---\n`;
     }
-
-    if (text.trim().length === 0) {
-      throw new PdfNotParsedError();
-    }
-
-    return this.postProcessText(text);
   }
 
-  private postProcessText(text: string): string {
-    const processedText = text
-      .split('\n') //Split the text into lines
-      .map((line) => line.trim()) //Remove spaces at start & end of each line
-      .filter((line, index, arr) => line !== '' || arr[index - 1] !== '') ////keep only one line if multiple lines are empty
-      .map((line) => line.replace(/\s{3,}/g, '   ')) ////remove whitespace in lines if there are more than 3 spaces
-      .join('\n'); //Join lines back into text
+  text = text.trim();
 
-    return processedText;
-  }
+  if (!text) throw new PdfNotParsedError();
+
+  return text;
+}
+
 
   async loadPdfFromUrl(url: string) {
     const MAX_SIZE = 5 * 1024 * 1024;
@@ -56,9 +57,7 @@ export class PdfParserService {
 
     const buffer = Buffer.from(response.data, 'binary');
 
-    if (buffer.length > MAX_SIZE) {
-      //if actual file size greaer than max-size
-
+    if (buffer.length > MAX_SIZE) {  //if actual file size greaer than max-size
       throw new PdfSizeError();
     }
 
